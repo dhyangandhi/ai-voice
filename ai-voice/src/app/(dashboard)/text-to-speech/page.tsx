a146cda1fce131fd42a160ca21296745830ea2ac
DhyanGandhi <dhyangandhi2008@gmail.com>
2026-05-19 22:19:31 +0530

    ai-model

--- ai-voice/src/app/(dashboard)/text-to-speech/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Metadata } from "next"
-
-export const metadata: Metadata = { title: "Text to speech"};
-export default function TextToSpeechPage() {
-    return (
-        <h1>Text to Speech Page</h1>
-    )
-}
--- ai-voice/src/features/text-to-speech/data/constants.ts
+++
@@ -1,2 +0,0 @@
-export const TEXT_MAX_LENGTH = 10000;
-export const COST_PER_UNIT = 0.0003;
